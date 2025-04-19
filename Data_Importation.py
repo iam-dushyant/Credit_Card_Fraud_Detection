ff131bb51acfba397a06a4d5d055ccf125286214
iam-dushyant <dushyant.khatri@icloud.com>
2025-04-19 16:58:49 +0100

    Changed dataset

--- Data_Importation.py
+++ Data_Importation.py
@@ -4,12 +4,12 @@ import kaggle
 import zipfile
 import os
 
-!kaggle datasets download -d mlg-ulb/creditcardfraud
+!kaggle datasets download -d ealaxi/paysim1
 
-zip_path = "creditcardfraud.zip"
+zip_path = "paysim1.zip"
 extract_path = os.getcwd()
 with zipfile.ZipFile(zip_path, 'r') as zip_ref:
     zip_ref.extractall(extract_path)
     
-df = pd.read_csv("creditcard.csv")
+df = pd.read_csv("PS_20174392719_1491204439457_log.csv")
 print("First 5 records:", df.head())

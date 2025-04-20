e1686ddb1124b04208235d04da7cd8c4fda83607
iam-dushyant <dushyant.khatri@icloud.com>
2025-04-20 14:25:48 +0100

    Modifying specifics for model development

--- Model_development.py
+++ Model_development.py
@@ -2,13 +2,17 @@ from Splitting_dataset import train_indices
 from Splitting_dataset import test_indices
 
 from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import confusion_matrix
 
-X_train = train_indices[::8]
-y_train = train_indices[:9]
-X_test = test_indices[::8]
-y_test = test_indices[:9]
+X_train = train_indices.iloc[:,:8]
+y_train = train_indices.iloc[:,9]
+X_test = test_indices.iloc[:,:8]
+y_test = test_indices.iloc[:,9]
 
 # Machine Learning with Linear Regression
-lin_reg = SGDClassifier()
-lin_reg.fit(train_data, train_label)
+classifier = SGDClassifier()
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
 
+# Checking prediction using Confusion Matrix
+conf_mat = confusion_matrix(y_train, y_pred)

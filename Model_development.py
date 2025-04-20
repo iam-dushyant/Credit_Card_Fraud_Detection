31b2706e9960019e8e0c0cfd4a144603f91a602f
iam-dushyant <dushyant.khatri@icloud.com>
2025-04-20 02:47:20 +0100

    Adding Model_development.py

---
+++ Model_development.py
@@ -0,0 +1,7 @@
+from Splitting_dataset import train_indices
+from Splitting_dataset import test_indices
+
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+lin_reg.fit()

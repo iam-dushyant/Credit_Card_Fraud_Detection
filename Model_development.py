2c7454ab0036fc5d7ee744461f04285fbf08d001
iam-dushyant <dushyant.khatri@icloud.com>
2025-04-20 14:12:52 +0100

    Modify Model_development to test SGDClassifier

--- Model_development.py
+++ Model_development.py
@@ -1,7 +1,14 @@
 from Splitting_dataset import train_indices
 from Splitting_dataset import test_indices
 
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import SGDClassifier
 
-lin_reg = LinearRegression()
-lin_reg.fit()
+X_train = train_indices[::8]
+y_train = train_indices[:9]
+X_test = test_indices[::8]
+y_test = test_indices[:9]
+
+# Machine Learning with Linear Regression
+lin_reg = SGDClassifier()
+lin_reg.fit(train_data, train_label)
+

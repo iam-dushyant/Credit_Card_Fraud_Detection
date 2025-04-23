1760f8461896ef8008a22339d4fe6109bfa0bc46
iam-dushyant <dushyant.khatri@icloud.com>
2025-04-23 22:10:58 +0100

    Debugging Model_Validation.py file

--- Model_Validation.py
+++ Model_Validation.py
@@ -3,3 +3,19 @@
 from sklearn.model_selection import StratifiedKFold
 from sklearn.base import clone
 
+from Model_development import classifier
+from Model_development import X_train, y_train
+
+skfolds = StratifiedKFold(n_splits=3, random_state=42)
+
+for train_index, test_index in skfolds.split(X_train, y_train):
+    clone_clf = clone(classifier)
+    X_train_folds = X_train[train_index]
+    y_train_folds = y_train[train_index]
+    X_test_fold = X_train[test_index]
+    y_test_fold = y_train[test_index]
+    
+    classifier.fit(X_train_folds, y_train_folds)
+    y_pred = clone_clf.predict(X_test_fold)
+    n_correct = sum(y_pred == y_test_fold)
+    print(n_correct / len(y_pred))
